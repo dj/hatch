5254601a7a1fa798f76c81e389f717898aa32a1d
DJ Hartman <danieljayhartman@gmail.com>
2015-02-23 17:53:11 -0500

    Hello, Backbone!

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,25 @@ var $ = require('jquery');
 var fs = require('fs');
 var _ = require('underscore');
 var Backbone = require('backbone');
+Backbone.$ = $;
 
-var HatchAppView = Backbone.View.extend({
-  el: '#hatch-app',
+var ResultsTableView = Backbone.View.extend({
+  el: $('#results-table'),
+  initialize: function() {
+    console.log('init');
+    _.bindAll(this, 'render');
+    this.render();
+  },
   defaultTweets: { user: '@djhrtmn', text: 'lorem ipsum sit amet delor' },
   template: _.template(fs.readFileSync('src/templates/tweet.html', 'utf8')),
   render: function(){
-    $(this.el).html(this.template(defaultTweets));
+    var html = this.template(this.defaultTweets);
+    console.log(html);
+    $(this.el).append(html);
     return this;
   }
-})
+});
 
 var app = {}
-app.Hatch = new HatchAppView();
+app.Hatch = new ResultsTableView();
+
