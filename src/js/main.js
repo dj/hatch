eb2da7ef24fbc20a71c44ef658d247f6f6a56609
DJ Hartman <danieljayhartman@gmail.com>
2015-02-23 23:07:41 -0500

    Use handlebars as a template lib instead of underscore.

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,31 @@ var $ = require('jquery');
 var fs = require('fs');
 var _ = require('underscore');
 var Backbone = require('backbone');
+var handlebars = require('handlebars');
+
+// Backbone assumes jQuery is in the global namespace
 Backbone.$ = $;
 
+// View for rendering a list of tweets
 var ResultsTableView = Backbone.View.extend({
   el: $('#results-table'),
   initialize: function() {
-    console.log('init');
+    // Bind the current context to the render method
     _.bindAll(this, 'render');
     this.render();
   },
-  defaultTweets: { user: '@djhrtmn', text: 'lorem ipsum sit amet delor' },
-  template: _.template(fs.readFileSync('src/templates/tweet.html', 'utf8')),
-  render: function(){
+  defaultTweets: {
+    tweets: [
+      { user: '@djhrtmn', text: 'lorem ipsum sit amet delor' },
+      { user: '@djhrtmn', text: 'lorem ipsum sit amet delor' },
+      { user: '@djhrtmn', text: 'lorem ipsum sit amet delor' },
+      { user: '@djhrtmn', text: 'lorem ipsum sit amet delor' },
+      { user: '@djhrtmn', text: 'lorem ipsum sit amet delor' }
+    ]
+  },
+  template: handlebars.compile(fs.readFileSync('src/templates/tweet.hbs', 'utf8')),
+  render: function () {
     var html = this.template(this.defaultTweets);
-    console.log(html);
     $(this.el).append(html);
     return this;
   }
