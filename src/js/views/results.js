ef3423bb92b355d184dc978c5055b9bd55dd9697
DJ Hartman <danieljayhartman@gmail.com>
2015-04-08 10:38:48 -0400

    Init bootstrap table plugin. Just going to use the CDN for now.

--- src/js/views/results.js
+++ src/js/views/results.js
@@ -17,24 +17,21 @@ module.exports = Backbone.View.extend({
       self.render()
     })
 
-    // Initialize Sub View
-    this.resultsListView = new ResultsListView();
-    this.resultsTableView = new ResultsTableView();
-
     // Render on model change
     this.listenTo(this.model, 'change', this.render);
   },
 
   events: {
-    "change #results-subview": "changeSubview"
+    "click #results-table-pane-btn": "initTable",
   },
 
-  changeSubview: function(e) {
-    if (e.target.value == 'Table') {
-      // Empty the subview, render the table subview
-    } else {
-      // Empty the subview, render the tweet preview subview
-    }
+  initTable: function(e) {
+    console.log('init table!')
+    console.log(_.map(this.model.changed))
+
+    $('#results-table').bootstrapTable({
+        data: _.map(this.model.changed)
+    });
   },
 
   template: function(data) {
