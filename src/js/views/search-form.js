deb65230a963ed23e770b07a7c093be635fe7596
DJ Hartman <danieljayhartman@gmail.com>
2015-04-08 13:52:09 -0400

    Quick and dirty spinnner, works for now.

--- src/js/views/search-form.js
+++ src/js/views/search-form.js
@@ -54,10 +54,39 @@ module.exports = Backbone.View.extend({
     $('#query').val([all, any, none, from].join(' '))
   },
 
+  // I'm sorry.
+  initSpinner: function() {
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 20, // The length of each line
+      width: 10, // The line thickness
+      radius: 30, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '15em', // Top position relative to parent
+      left: '45%' // Left position relative to parent
+    };
+
+    var spinner = new Spinner(opts).spin();
+    $('#results-container').append(spinner.el);
+  },
+
   search: function(e) {
     e.preventDefault();
     // TODO: clean up to prevent zombie views
-    $('#results-container').empty();
+    var resultsContainer = $('#results-container')
+    resultsContainer.empty();
+
+    // Loading Spinner
+    this.initSpinner();
 
     // A list of pairs of
     // string: a url query parameter + "="
