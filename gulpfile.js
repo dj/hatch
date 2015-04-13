ebcade26f0099f53abe01758a7d94ea19d30b3ff
yacn <admin@yacn.me>
2015-04-13 17:13:12 -0400

    updates gulpfile to watch conditionally

--- gulpfile.js
+++ gulpfile.js
@@ -11,17 +11,29 @@ var less = require('gulp-less');
 var path = require('path');
 
 // Browserify
-var bundler = watchify(browserify('./src/js/main.js', watchify.args));
+var bundler = browserify('./src/js/main.js', watchify.args);
 bundler.transform('brfs'); // implements node's fs.readFileSync for browserify
 
+// Asset locations
+var not_tests = '!src/test/**';
+var html_files = 'src/**/*.html';
+var pngs = 'src/**/*.png'
+var files = [
+  not_tests,
+  html_files,
+  pngs
+]
+var libs = 'src/js/vendor/**/*';
+var fonts = 'src/fonts/**/*';
+var less_files = './src/less/**/*.less';
+
 // Tasks
-gulp.task('default', ['copy', 'vendor', 'fonts', 'less', 'less-watch', 'js']);
+gulp.task('default', ['watch']);
 gulp.task('js', bundle);
 gulp.task('copy', copyFiles);
 gulp.task('fonts', copyFonts);
 gulp.task('vendor', copyVendoredLibs);
-
-bundler.on('update', bundle); // on any dep update, runs the bundler
+gulp.task('build', ['copy', 'less', 'fonts', 'vendor', 'js'], function () { return;});
 
 function bundle() {
   return bundler.bundle()
@@ -35,7 +47,7 @@ function bundle() {
 }
 
 gulp.task('less', function() {
-  return gulp.src('./src/less/**/*.less')
+  return gulp.src(less_files)
     .pipe(less({
       paths: [
         'node_modules/bootstrap/less'
@@ -44,34 +56,28 @@ gulp.task('less', function() {
     .pipe(gulp.dest('./build/css'));
 })
 
-gulp.task('less-watch', ['less'], function() {
-  gulp.watch('./src/less/*.less', ['less']);
+gulp.task('watch', ['less', 'copy', 'vendor', 'fonts', 'js'], function () {
+  var watched_bundler = watchify(bundler);
+  watched_bundler.on('update', bundle); // on any dep update, runs the bundler
+
+  gulp.watch(less_files, ['less']);
+  gulp.watch(files, ['copy']);
+  gulp.watch(libs, ['vendor']);
+  gulp.watch(fonts, ['fonts']);
 });
 
 function copyFiles() {
-  var files = [
-    'src/**/*.html',
-    'src/**/*.png'
-  ]
-
   gulp.src(files)
-    .pipe(watch(files))
     .pipe(gulp.dest('build'));
 }
 
 function copyVendoredLibs() {
-  var libs = 'src/js/vendor/**/*';
-
   gulp.src(libs)
-    .pipe(watch(libs))
     .pipe(gulp.dest('build/js/vendor'));
 }
 
 function copyFonts() {
-  var fonts = 'src/fonts/**/*';
-
   gulp.src(fonts)
-    .pipe(watch(fonts))
     .pipe(gulp.dest('build/fonts'));
 }
 
